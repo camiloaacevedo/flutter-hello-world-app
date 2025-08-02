3f827943cfda57fc1438087ff9836864b304225e
camiloaacevedo <ccuevasa@uninorte.edu.co>
2025-08-01 22:38:48 -0500

    First commit

---
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class CounterFunctionsScreen extends StatefulWidget {
+  const CounterFunctionsScreen({super.key});
+
+  @override
+  State<CounterFunctionsScreen> createState() => _CounterFunctionsScreenState();
+}
+
+class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter Functions'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+            icon: Icon(Icons.refresh_rounded),
+          ),
+        ],
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+            ),
+
+            Text(
+              'Click${clickCounter == 1 ? '' : 's'}',
+              style: TextStyle(fontSize: 25),
+            ),
+
+            // if (clickCounter == 1)
+            //   Text('Click', style: TextStyle(fontSize: 25)),
+
+            // if (clickCounter != 1)
+            //   Text('Clicks', style: TextStyle(fontSize: 25)),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          CustomButton(icon: Icons.refresh_rounded),
+
+          SizedBox(height: 10),
+
+          CustomButton(icon: Icons.exposure_minus_1_outlined),
+
+          SizedBox(height: 10),
+
+          CustomButton(icon: Icons.plus_one),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final onPressed;
+
+  const CustomButton({super.key, required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: StadiumBorder(),
+
+      onPressed: () {},
+      child: Icon(icon),
+    );
+  }
+}
